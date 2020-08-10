35059e63086cf449b87d36f044f3b5661ea1971f
zlemondra <44854321+zlemondra@users.noreply.github.com>
2020-08-09 20:35:45 -0700

    PeerNode completed

--- Peer-2-Peer/src/PNode.java
+++ Peer-2-Peer/src/PNode.java
@@ -7,12 +7,12 @@ import java.util.concurrent.ConcurrentHashMap;
  * what this class is, is the part of the peer node that does all the client side of request like pushing and pulling the files
  */
 public class PNode implements Runnable{
-	private ConcurrentHashMap<String, Socket> socketHashMap;
+	private HashMap<String, Socket> socketHashMap;
 	private Socket peerSocket;
 	Scanner input = new Scanner(System.in);
 	File file;
 	// constructor of peer node takes in the hash map used for the DHT to keep files
-	PNode(ConcurrentHashMap<String, Socket> given){
+	PNode(HashMap<String, Socket> given){
 		socketHashMap = given;
 	}
 	/*
@@ -24,63 +24,126 @@ public class PNode implements Runnable{
 	// this is the part of the peer node that you can do the client portion and then run for threads
 	@Override
 	public void run() {
-		
+		// taking in client/user input 
 		String answer = "";
 		while(answer != "Leave") {
 			System.out.println("Enter what you want to do:\n"
-					+ "Push\n"
-					+ "Pull\n"
+					+ "Push\n"//sending a file to the server
+					+ "Pull\n"// requesting server
 					+ "Leave\n");
 			answer = input.next();
 			
 			// compare answer
 			switch(answer) {
 			case "Push":
-				push();
+				try {//needs exception because of use of files
+					push();//invoke pushing method
+				} catch (IOException e) {					
+					e.printStackTrace();
+				}
+				break;
 			case "Pull":
-				pull();
-				
+				pull();//invoke pull method
+				break;				
 			}
 		}		
 	}
-	private void push() {
+	private void push() throws IOException {
+		//ask for key pair to create new hash map 
+		System.out.println("Enter a new key: ");
+		String key = input.next();
+		
+		//creating file and writing contents of said new file
+		System.out.println("Enter your name for new file: ");
+		String fileName = input.nextLine();
+		file = new File(fileName +".txt");
+		file.createNewFile();
+		System.out.println("Enter Contents of file");
+		String contents = input.nextLine();
+		FileWriter myWriter = new FileWriter(file);
+	    myWriter.write(contents);
+	    myWriter.close();
+		//find the hashValue, where to put this KEY,VALUE
+		peerSocket = getServerSocket(key);
+		String fileAndContent = fileName + contents;
+		//put the value if local server, put into local hash table
+		//else, put into other server, by connecting to other server using sockCommunicateStream
+		if(peerSocket == null)
+		{
+			boolean result = PNode_ServSide.push(key,fileAndContent);
+			if(result)
+					System.out.println("Success");
+				else
+					System.out.println("Failure");
+		}
+		else{			
+			socketCommunicate(peerSocket,"Push",key, file);
+		}
 		
 		
 	}
 	private void pull() {
 		System.out.println("Enter Key");
 		String key = input.next();
+		//get the Hashvalue where to get the value from
+		peerSocket = getServerSocket(key);
 		
-	
+		if(peerSocket == null)//search in local hash map,
+			System.out.println("The value is: "+PNode_ServSide.pull(key));
+		
+		else//else, find from other server
+			socketCommunicate(peerSocket,"Pull",key, file);
+					
 		
 	}
-	/* The entire message i.e KEY + VALUE is of 1024 bytes
-	 * out of which KEY is of 24 Bytes, here we pad the remaning bytes of the key with "*" 
-	 * while sending we send entire 1024 bytes*/
-	public String padKey(String key)
+	// method to get socket from hash map
+	public Socket getServerSocket(String key)
 	{
-		for(int i=key.length();i<24;i++)
-		{
-			key+="*";
-		}
-		return key;
-	}
-	/* The entire message i.e KEY + VALUE is of 1024 bytes
-	 * out of which VALUE is of 1000 Bytes, here we pad the remaning bytes of the value with "*" 
-	 * while sending we send entire 1024 bytes*/
-	public String padValue(String value)
-	{
-		for(int i=value.length();i<1000;i++)
-		{
-			value+="*";
-		}
-		return value;
-	}
-	public Socket getServerSocket(String Key)
-	{
-		String hashValue = "server"+Math.abs((Key.hashCode())%8);
-		Socket socket = socketHashMap.get(hashValue);			
+		Socket socket = socketHashMap.get(key);			
 		return socket;
 	}
-	
+
+	/*This method is used to connect between sockets i.e. Servers, and send and receive
+	 * message and Communicate for key/value pair to push or pull */
+	public void socketCommunicate(Socket sock, String userInput, String key, File file){
+		try{
+			//make send and receive for sockets to communicate
+			DataInputStream in = new DataInputStream(sock.getInputStream());
+			DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+			//reading file to push
+			String data= "";//contents of file
+			try {
+			      Scanner reader = new Scanner(file);
+			      data = reader.nextLine();			   
+			      reader.close();
+			    } catch (FileNotFoundException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+			}
+			String keyPair =key +","+ file.getName()+"," + data;
+			//send the server the choice and key/value
+			out.writeUTF(userInput);
+			out.writeUTF(keyPair);
+			//this reads the file when pulling it
+			if(userInput.equals("Pull")){
+				String[] contents = in.readUTF().split(",");
+				System.out.println("File: "+contents[1]+
+						"\nFile Contents:\n"+ contents[2]);
+			}
+			//tells you if you successfully pull it
+			if(userInput.equals("Push")){	
+				String result = in.readUTF();
+				if(result.equals("true"))
+					System.out.println("Success");			
+				else
+					System.out.println("Failure");
+			}
+			
+		} catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
 }
---
+++ Peer-2-Peer/src/PNode_ServSide.java
@@ -0,0 +1,95 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PNode_ServSide {
+	private Socket clientSocket;
+	public static HashMap<String, String> DHT = new HashMap<String, String>();//distributed Hash Table in the form of a hashp
+	private boolean loop = true;
+
+	public PNode_ServSide(Socket client) {
+		// takes in the client socket to do server tasks based on the client's request
+		clientSocket = client;
+	}
+
+	public void run(){
+		try{
+			//for communication over sockets between Client and server
+			DataInputStream in = new DataInputStream(clientSocket.getInputStream());//input
+			DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());//output
+
+			while(loop){
+				try{/*lauren if you're wondering why I have all theses try and catches, it's because of its auto-generated because I'm rerading data from 
+					the communication and you need a try-catch in case there's no data
+				*/
+					//read the choice according to client input
+					String answer = in.readUTF();
+
+					switch(answer){
+						case "Push":	
+							//read the key value pair from client
+							String readKey = in.readUTF();
+							
+							//split the value from Key and file by ","
+							String[] hashKey = readKey.split(",");
+							String fileAndContent = hashKey[1]+ hashKey[2];//String of the file
+							boolean pushResult = push(hashKey[0],fileAndContent);
+							out.writeUTF(String.valueOf(pushResult));//writing if pushing the file was successful
+							break;
+	
+						case "Pull":	
+
+							String key = in.readUTF();
+							String value = pull(key);
+							
+							/*Comment out below for Performance Evaluation*/
+							
+							/*Check if value is null return NULL to client, else return the actual VALUE for the KEY*/
+							if(value == null)
+								out.writeUTF("null");
+							else
+								out.writeUTF(value);
+							break;
+	
+						case "Leave":					
+							System.out.println("Client Disconnected");
+							loop = false;	//exit from the while(true) loop
+							//System.exit(0);
+							break;
+					}
+
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}//end of while
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+
+	//Put function to put the KEY VALUE pair into the HashTable
+	public static boolean push(String key, String file){
+		//if put into hashtable is successful then true, and display over which server the hashtable was pushed
+		if((DHT.put(key, file))!="null"){
+			System.out.println("Distributed Hash Table:\n"+DHT);
+			return true;
+		}
+		else{
+				return false;
+		}
+	}
+	
+	//Get function to get the VALUE from the Specified KEY, if value is null it will return NULL
+	public static String pull(String key)
+	{		
+		String value = DHT.get(key);
+		return value;
+	}
+	
+
+}
