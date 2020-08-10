f45730e606c6507d536068f52370e5e8ae1e9ab4
zlemondra <44854321+zlemondra@users.noreply.github.com>
2020-08-09 18:14:43 -0700

    no message handler

--- Peer-2-Peer/src/PNode.java
+++ Peer-2-Peer/src/PNode.java
@@ -1,69 +1,86 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.net.*;
 import java.io.*;
-
-public class PNode {
-	int port;//port number
-	String location;//Directory Location
-	
-	PNode(int inputPort) throws IOException{
-		port = inputPort;
-		location = port + "";
-		//start the client server
-		
-		/*Hey Lauren, if you're wondering what this is, its the thread that starts he client server
-		 * I had to make it a run method in the new thread object so the thread wouldn't run in an endless
-		 * loop. try and catch was a n automated 	
-		 * hello
-		*/
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					startServer(port);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		).start();				
+import java.util.concurrent.ConcurrentHashMap;
+/*
+ * Hello Professor Giaccolne,
+ * what this class is, is the part of the peer node that does all the client side of request like pushing and pulling the files
+ */
+public class PNode implements Runnable{
+	private ConcurrentHashMap<String, Socket> socketHashMap;
+	private Socket peerSocket;
+	Scanner input = new Scanner(System.in);
+	File file;
+	// constructor of peer node takes in the hash map used for the DHT to keep files
+	PNode(ConcurrentHashMap<String, Socket> given){
+		socketHashMap = given;
 	}
 	/*
 	 * method called upon in the node constructor
 	 * starts the client server by starting a thread to run the message hander to process request
 	 */
-	void startServer(int temp) throws IOException { // takes in the port number as a parameter	
-		ServerSocket serv = new ServerSocket(0); //server socket
-		System.out.println("Server started at " + InetAddress.getLocalHost() + ":" + port);
-		while(true) {
-			//listening for a connection request
-			Socket connect = serv.accept();//accepting the request
-			//message handler object that takes the client request
-			MessageHandler request = new MessageHandler(connect); //takes the connection socket as a parameter to process that request 						
-			// thread object for request handler
-			Thread thread = new Thread((Runnable) request);
-			//now start the thread
-			thread.start();
+
+	
+	// this is the part of the peer node that you can do the client portion and then run for threads
+	@Override
+	public void run() {
+		
+		String answer = "";
+		while(answer != "Leave") {
+			System.out.println("Enter what you want to do:\n"
+					+ "Push\n"
+					+ "Pull\n"
+					+ "Leave\n");
+			answer = input.next();
 			
-		}
+			// compare answer
+			switch(answer) {
+			case "Push":
+				push();
+			case "Pull":
+				pull();
+				
+			}
+		}		
 	}
-	void send(String file, String host, int port) throws UnknownHostException, IOException {
-		Socket sock = new Socket(host, port);//creating the socket with the machine name and port number from parameter
-		PrintWriter output = new PrintWriter(sock.getOutputStream(), true);
-		/*
-		 * socket getOutputSream method:
-		 * Returns an output stream for this socket. If this socket has an associated channel then 
-		 * the resulting outputstream delegates all of its operations to the channel. If the channel is in 
-		 * non-blocking mode then the output stream's write
-		 * operations will throw an IllegalBlockingModeException. 
-		 * Closing the returned OutputStreamwill close the associated socket
-		 */
-		output.println(file); //printing the file being sent
-		//gotta close up the connections
-		output.close(); sock.close();
+	private void push() {
+		
 		
 	}
+	private void pull() {
+		System.out.println("Enter Key");
+		String key = input.next();
+		
 	
-
+		
+	}
+	/* The entire message i.e KEY + VALUE is of 1024 bytes
+	 * out of which KEY is of 24 Bytes, here we pad the remaning bytes of the key with "*" 
+	 * while sending we send entire 1024 bytes*/
+	public String padKey(String key)
+	{
+		for(int i=key.length();i<24;i++)
+		{
+			key+="*";
+		}
+		return key;
+	}
+	/* The entire message i.e KEY + VALUE is of 1024 bytes
+	 * out of which VALUE is of 1000 Bytes, here we pad the remaning bytes of the value with "*" 
+	 * while sending we send entire 1024 bytes*/
+	public String padValue(String value)
+	{
+		for(int i=value.length();i<1000;i++)
+		{
+			value+="*";
+		}
+		return value;
+	}
+	public Socket getServerSocket(String Key)
+	{
+		String hashValue = "server"+Math.abs((Key.hashCode())%8);
+		Socket socket = socketHashMap.get(hashValue);			
+		return socket;
+	}
 	
 }
